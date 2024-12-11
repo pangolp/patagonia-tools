f81105c3b5ae3ba08841f7e315d593a81e5ebead
Pagani Walter <paganiwalter@gmail.com>
2024-12-11 09:26:05 -0300

    Crafteo de cristales rotos custom

--- Contents/mods/PatagoniaTools/media/lua/server/patagonia_recipecode.lua
+++ Contents/mods/PatagoniaTools/media/lua/server/patagonia_recipecode.lua
@@ -6,3 +6,18 @@ function Recipe.OnGiveXP.Mechanics1(recipe, ingredients, result, player)
     local xpAfter = player:getXp():getXP(Perks.Mechanics)
     player:setHaloNote(string.format("Mechanics: %.2f", (xpAfter - xpBefore)))
 end
+
+function Recipe.GetItemTypes.BrokenGlassCustom(scriptItems)
+    local itemNames = {
+        "Base.brokenglass_1_1",
+        "Base.brokenglass_1_2",
+        "Base.brokenglass_1_3"
+    }
+
+    for _, itemName in ipairs(itemNames) do
+        local item = ScriptManager.instance:getItem(itemName)
+        if item then
+            scriptItems:add(item)
+        end
+    end
+end
